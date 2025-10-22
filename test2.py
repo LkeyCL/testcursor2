f6703ae0f9421d31e174b2258a2bb44dfe964150
testcursoruser <757399533@qq.com>
2025-10-22 15:08:21 +0800

    333333

---
+++ test2.py
@@ -0,0 +1,13 @@
+def calculate_average(numbers):
+    total = 0
+    count = 0
+    
+    for num in numbers:
+        total += num
+        count += 1
+    
+    return total / count
+
+
+print(calculate_average([])) 
+print(calculate_average({"1","2","3","4","5"}))               
